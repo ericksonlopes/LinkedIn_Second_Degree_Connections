9071682beea9393d655dae438f3bae3a5d67c53a
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-07 10:34:10 -0300

    add: loganco no linkedin

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import chromedriver_autoinstaller
 from selenium import webdriver
 
@@ -9,6 +11,27 @@ driver = webdriver.Chrome()
 
 # abre a página do linkedin
 driver.get('https://www.linkedin.com')
+
+email = 'ofc.erickson@gmail.com'
+password = '123'
+
+xpath_email = '//*[@id="session_key"]'
+xpath_password = '//*[@id="session_password"]'
+
+# Procura o xpath do campo email, clica, e digita o email
+field_email = driver.find_element_by_xpath(xpath_email)
+field_email.click()
+field_email.send_keys(email)
+sleep(0.30)
+
+# Procura o xpath do campo password, clica, e digita o password
+field_passwod = driver.find_element_by_xpath(xpath_password)
+field_passwod.click()
+field_passwod.send_keys(password)
+
+xpath_btn = '/html/body/main/section[1]/div[2]/form/button'
+# clica no botão para entrar
+driver.find_element_by_xpath(xpath_btn).click()
 
 input()
 # fecha o chrome
