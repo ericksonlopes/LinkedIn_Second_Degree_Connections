1197f6151ccc5251d1151db462acb1ea6b18b8f4
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-03-07 10:17:39 -0300

    add: requeriments, abrindo página do linkedin

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 import chromedriver_autoinstaller
+from selenium import webdriver
 
+# instala o driver do Chrome
 chromedriver_autoinstaller.install()
 
+# instancia o obj do webdriver chrome
+driver = webdriver.Chrome()
+
+# abre a página do linkedin
+driver.get('https://www.linkedin.com')
+
+input()
+# fecha o chrome
+driver.close()
